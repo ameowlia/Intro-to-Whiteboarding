68e74599fc82a7049115f3a4afe92e9920f3fdd0
julian <julian.gallegos@gmail.com>
2015-02-10 21:14:07 -0800

    ruby add node to list method

--- solutions/julian_linked_list.rb
+++ solutions/julian_linked_list.rb
@@ -17,20 +17,46 @@ class LinkedList
 	end
 
 	def add_node_to_list(value)
-		if size == 0
+		if @head == nil
 			@head = Node.new(value)
 			@size += 1
 		else
-
-
-
-	end
-
-	def add_at_index(value, index)
-		if size == 0
-			@head = Node.new(value)
-			@size += 1
+			current_node = @head
+			index = 0
+			while index < @size
+				if current_node.pointer == nil
+				current_node.pointer = Node.new(value)
+				else
+					index += 1
+				end
+			end
+				@size += 1
 		end
 	end
 
+
+	def printOutLinkList
+		index = 0
+		current_node = @head
+		while current_node != nil
+			p "index: #{index} value: #{current_node.value}"
+			current_node = current_node.pointer
+			index += 1
+		end
+	end
+
+
 end
+
+
+
+myList = LinkedList.new
+
+
+
+myList.add_node_to_list(2)
+
+myList.add_node_to_list(3)
+
+myList.printOutLinkList
+
