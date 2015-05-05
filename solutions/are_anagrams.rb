3f255a945a6c0e75343c435e0307bae1c155f4a2
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-05 16:06:34 -0700

    Added are_anagrams.rb

---
+++ solutions/are_anagrams.rb
@@ -0,0 +1,4 @@
+# Write the method are_anagrams? that takes two strings as arguments and returns true if they are anagrams of each other or false if they are not.
+
+# Explain the time and space complexity of your algorithm.
+
