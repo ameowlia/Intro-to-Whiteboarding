96be5fbd59057f48d67e2e8b8f5147f1866a5524
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-05 16:25:42 -0700

    Added 2nd solution to are_anagrams?

--- solutions/are_anagrams.rb
+++ solutions/are_anagrams.rb
@@ -15,7 +15,24 @@ end
 # b) Time and space complexity:
 # 0(n) time and 0(1) space
 
+# c) Bonus: without using the Ruby #sort method
+
+def are_anagrams_2?(string1, string2)
+  return false if string1.length != string2.length
+  hash = Hash.new(0)
+  string1.chars.each {|character| hash[character] += 1}
+  string2.chars.each do |letter|
+    return false if hash[letter] < 1
+    hash[letter] -= 1
+  end
+  hash.each_value {|v| return false if v != 0}
+  true
+end
+
 
 # TEST
 p are_anagrams?("dbc", "cdb") == true
 p are_anagrams?("dbc", "pizzarules") == false
+
+p are_anagrams_2?("dbc", "cdb") == true
+p are_anagrams_2?("dbc", "pizzarules") == false
