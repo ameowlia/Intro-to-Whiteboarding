c1196d7cfcd11efca26b39a9b691821ea77244e1
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-05 14:45:43 -0700

    Added solution to 1st part, is_prime?

--- solutions/is_prime_amadou.rb
+++ solutions/is_prime_amadou.rb
@@ -10,3 +10,31 @@
 # is_prime?(2) #=> true
 # is_prime?(6) #=> false
 # Bonus: Can you improve your algorithm by memoizing it?
+
+# SOLUTION
+# A) is_prime?
+# PSEUDOCODE:
+# is_prime? takes a number as input, which we want to check whether it is prime or not.
+# We return false if number is < 2
+# We iterate from 2 upto the square root of number (included). If we incounter an integer for which the input
+# is divisibe by, we return false. Otherwise we return true
+
+# WHY upto square root of number?
+# if a number n = a*b and 1 < a <= b, then a^2 <= ab <= b^2
+# therefore a^2 <=n <= b^2, then a <=sqrt(n) <= b
+# there has to be a prime factor till sqrt(n) if n is composite. if there is no prime factor till sqrt(n), n is itself a prime.
+def is_prime?(number)
+  return false if number < 2
+  i = 2
+  while i <= Math.sqrt(number) # See below why we stop at square root of number
+    return false if number % i == 0
+    i += 1
+  end
+  true
+end
+
+# TEST DRIVE:
+p is_prime?(2) == true
+p is_prime?(-4) == false
+p is_prime?(11) == true
+p is_prime?(97) == true
