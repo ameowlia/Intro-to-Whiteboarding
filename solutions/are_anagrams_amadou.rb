097afc7fada0e90dfa562a66c5d94b953cbe5d46
AmadouSallah <amadou.saikou.sallah@gmail.com>
2015-05-05 18:07:00 -0700

    Added Pseudocodes to methods in are_anagrams_amadou.rb

--- solutions/are_anagrams_amadou.rb
+++ solutions/are_anagrams_amadou.rb
@@ -7,6 +7,10 @@
 # SOLUTION
 
 # a) Using Ruby's sort method:
+# PSEUDOCODE
+# We return false if the 2 strings have different lengths
+# We split each input string, sort the arrays that result from the string splitting, and compare the 2 sorted arrays.
+
 def are_anagrams?(string1, string2)
   return false if string1.length != string2.length
   return string1.chars.sort == string2.chars.sort
@@ -16,6 +20,15 @@ end
 # 0(n) time and 0(1) space
 
 # c) Bonus: without using the Ruby #sort method
+
+# PSEUDOCODE
+# We return false if the 2 strings have different lengths
+# We define a hash with default value of 0
+# We add to the hash each character from the 1st input string
+# We iterate through the 2nd input string, and for each character in it, we return false if the hash
+# value of that character is less than 1; otherwise, we decrement it by 1
+# We iterate through the hash and return false if any of the values is not 0
+# We return true at the end
 
 def are_anagrams_2?(string1, string2)
   return false if string1.length != string2.length
