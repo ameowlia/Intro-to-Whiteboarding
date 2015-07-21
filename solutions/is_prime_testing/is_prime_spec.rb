9fa68170f45ff3455252ca157e5b209f56b63e5b
Teresa Martyny <teresamartyny@gmail.com>
2015-07-20 20:54:10 -0700

    add in tests for each method

--- solutions/is_prime_testing/is_prime_spec.rb
+++ solutions/is_prime_testing/is_prime_spec.rb
@@ -22,3 +22,36 @@ RSpec.describe PrimeNum, "#is_prime?" do
   expect(test.is_prime?).to be true
  end
 end
+
+RSpec.describe PrimeNum, "#less_than_two?" do
+ it "returns true if a number is less than 2" do
+  test = PrimeNum.new(1)
+  expect(test.less_than_two?).to be true
+ end
+ it "returns false if a number is greater than 2" do
+  test = PrimeNum.new(3)
+  expect(test.less_than_two?).to be false
+ end
+end
+
+RSpec.describe PrimeNum, "#less_than_four?" do
+ it "returns true if a number is less than 4" do
+  test = PrimeNum.new(1)
+  expect(test.less_than_four?).to be true
+ end
+ it "returns false if a number is greater than 4" do
+  test = PrimeNum.new(5)
+  expect(test.less_than_four?).to be false
+ end
+end
+
+RSpec.describe PrimeNum, "#remaining_factors_prime?" do
+ it "returns false if it's a large non-prime number" do
+  test = PrimeNum.new(542)
+  expect(test.remaining_factors_prime?).to be false
+ end
+ it "returns true if it's a large prime number" do
+  test = PrimeNum.new(541)
+  expect(test.remaining_factors_prime?).to be true
+ end
+end
