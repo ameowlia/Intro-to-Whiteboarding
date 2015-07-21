f221e1a42667e91b700ed68dde558d7f2bcf01b8
Teresa Martyny <teresamartyny@gmail.com>
2015-07-20 20:53:47 -0700

    add in test for non-numbers. add in specific tests for each method.

--- solutions/is_prime_testing/is_prime_spec.rb
+++ solutions/is_prime_testing/is_prime_spec.rb
@@ -1,6 +1,10 @@
 require_relative 'is_prime_martyny'
 
-RSpec.describe PrimeNum do
+RSpec.describe PrimeNum, "#is_prime?" do
+ it "returns false if a non-number is parsed" do
+  test = PrimeNum.new("yo")
+  expect(test.is_prime?).to be false
+ end
  it "returns true if a small number is prime" do
   test = PrimeNum.new(2)
   expect(test.is_prime?).to be true
