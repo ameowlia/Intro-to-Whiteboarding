e85e6b837009e365e0ca17592a133d8082e99773
Teresa Martyny <teresamartyny@gmail.com>
2015-07-21 08:00:15 -0700

    use match array to be more explicit. add distribute candy test

--- solutions/candy_jar/candy_jar_spec.rb
+++ solutions/candy_jar/candy_jar_spec.rb
@@ -6,15 +6,13 @@ test_a = Distributor.new(distributions: distribution_a)
 test_b = Distributor.new(distributions: distribution_b)
 
 RSpec.describe Distributor, "#distribute_candy" do
-  # it ""
-  # expect
+  it "should increase the value of total_candies to 800" do
+    expect { test_a.distribute_candy }.to change{test_a.total_candies}.from(0).to(800)
+  end
 end
 RSpec.describe Distributor, "#calculate_average" do
   it "should return an integer" do
     expect(test_a.calculate_average).to be_a(Integer)
-  end
-  it "should distribute the candy and return the average number of candy per jar" do
-    expect(test_a.calculate_average).to eq(160)
   end
   it "should distribute the candy and return the average number of candy per jar" do
     expect(test_b.calculate_average).to eq(9)
@@ -22,7 +20,7 @@ RSpec.describe Distributor, "#calculate_average" do
 end
 RSpec.describe Distributor, "#distributions" do
   it "Should return the initial array minus the first element." do
-    expect(test_a.distributions).to eq([[1,2,100],[2,5,100],[3,4,100]])
+    expect(test_a.distributions).to match_array([[1,2,100],[2,5,100],[3,4,100]])
   end
   it "Should return the initial array minus the first element." do
     expect(test_b.distributions).to match_array([[1,2,4],[2,3,8],[1,1,5]])
